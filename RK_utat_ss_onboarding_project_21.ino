099ad48ccf933a4c40bb1d01e3191c4105735313
RudainaK <rkhalil828@gmail.com>
2020-11-13 04:40:50 +0900

    fixed conversion, timer still won't delay, waiting for feedback from Brytni

--- RK_utat_ss_onboarding_project_21.ino
+++ RK_utat_ss_onboarding_project_21.ino
@@ -1,14 +1,14 @@
 #include <avr/eeprom.h>
 #include <avr/io.h>
 
-int value = 0; //will be voltage from pin A0
-uint8_t temp = 0; //will store temperature in C after conversion
+float value = 0; //will be voltage from pin A0
+float temp = 0; //will store temperature in C after conversion
 
 void timer_delay(uint8_t seconds) //my user defined delay function
 {
   TCNT1= 0; //Timer1 set to 0 initially?
-  TCCR1A = (1<<WGM12); //CTC mode, assuming the rest are 0 by default? 
-  TCCR1B = (1<<CS10) | (1<<CS12); //sets prescaler to 1024
+  TCCR1A = (0<<WGM13) | (1<<WGM12) | (0<<WGM11) | (0<<WGM10); //CTC mode, assuming the rest are 0 by default? 
+  TCCR1B = (1<<CS10) | (0<<CS11) | (1<<CS12); //sets prescaler to 1024
   
   OCR1A = (seconds/(1/15625));
   //number of ticks in order to get to seconds
@@ -19,6 +19,7 @@ void timer_delay(uint8_t seconds) //my user defined delay function
   
   //at this point the timer should reset on its own 
   //because its in CTC mode, function returns here 
+  
 }
 
 void setup()
@@ -26,7 +27,7 @@ void setup()
   Serial.begin(9600);
    
   value = analogRead(A0);
-  temp = ((value*4.9)/10);
+  temp = round(((value/1024)*5 - 0.5)/0.01);
   
   //in the set up, it'll run this once, so this is for writing the first time
   //in the loop function it will update
@@ -51,7 +52,8 @@ void loop()
   //then based off of the TMP36 datasheet, 
   //I need to divide by 10 mV/C to get the temperature
   
-  temp = ((value*4.9)/10 - 500);//need offset voltage???
+  temp = round(((value/1024)*5 - 0.5)/0.01);
+  
   
   if(temp > 20){
     eeprom_update_byte((uint8_t*)0,temp);
